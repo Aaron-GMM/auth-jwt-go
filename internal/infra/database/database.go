3dcd92586abcb2c167748877898184f4b953fbc2
Aaron Gibran <aarongibran13@gmail.com>
2026-03-03 15:21:35 -0300

    bugfix(Cyclci Imports): resolved cyclic imports in infra security

--- internal/infra/database/database.go
+++ internal/infra/database/database.go
@@ -9,22 +9,23 @@ import (
 )
 
 var Db *gorm.DB
+var err error
 
 func InitializeDatabase(dsn string) (*gorm.DB, error) {
-	logger := config.GetLogger("postegresql")
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	logger := config.GetLogger("database")
+	Db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		logger.ErrorF("Error connecting to database: %s", err.Error())
 		return nil, err // Pare aqui se der erro!
 	}
 
 	// Só migra se a conexão for bem-sucedida
-	err = db.AutoMigrate(&entity.User{})
+	err = Db.AutoMigrate(&entity.User{})
 	if err != nil {
 		logger.ErrorF("Error migrating database: %s", err.Error())
 		return nil, err
 	}
 
-	return db, nil
+	return Db, nil
 
 }
